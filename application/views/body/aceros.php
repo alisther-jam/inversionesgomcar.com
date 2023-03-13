fc362191764d920468fa379ff4ea5359cdd5a7d9
alisther-jam <117322035+alisther-jam@users.noreply.github.com>
2023-03-13 12:50:02 -0500

    aceros

--- application/views/body/aceros.php
+++ application/views/body/aceros.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="imagenes.css">
 <section id="cabecera">
     <div class="container">
         <h1>Servicios</h1>
@@ -5,6 +6,15 @@
 </section>
 <section id="contenido">
     <div class="container">
+        <div><h1><b>ACERO INOXIDABLE</b></h1></div>
         <div>hola mundo</div>
+        <div class = "d-block float-left">Lorem ipsum dolor sit amet, consectetur  <br> 
+            adipisicing elit. Animi sint veritatis possimus quia ex tempore, <br> 
+            eaque architecto facilis? Iusto laborum quibusdam assumenda incidunt <br> 
+            eius quasi officiis consectetur placeat quos accusamus.</div>
+        <div class = "contImg">
+            <img src="img/acero-inoxidable/img3-grande.jpg" width = "500" class = "img-fluid float-right d-block" alt="">
+            <p>Exibidora grande</p>
+        </div>
     </div>
 </section>
